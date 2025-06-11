f4d5630195c2504074d6e743294de95a780b3f84
Etasin16 <e.tasin02@gmail.com>
2025-06-11 13:23:06 +0600

    n

--- lithora-app.py
+++ lithora-app.py
@@ -224,7 +224,7 @@ elif st.session_state.page == "cia":
         for i in range(11):
             fcn = i/10
             x_m,y_m = zip((-0.1, 0+fcn), (-0.1, (math.sqrt(3)/2)))
-            ax.plot(x_m, y_m, marker='|', color='red',lw=1)
+            ax.plot(x_m, y_m, marker='<', color='red',lw=1)
 
         # Axis labels
         ax.text(0.5, math.sqrt(3)/2 + 0.05, 'A (Al₂O₃)', ha='center', fontsize=14)
