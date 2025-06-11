a3a8285f36bab644f9bede70ffd783d4a22f7f14
Etasin16 <e.tasin02@gmail.com>
2025-06-11 15:52:27 +0600

    NMTD

--- lithora-app.py
+++ lithora-app.py
@@ -252,7 +252,7 @@ elif st.session_state.page == "cia":
         ax.text(0.8, 0.52, 'k-feldspar', ha='center', fontsize=7,color = "gray")
 
           # Arrows
-        plt.arrow(0.28, 0.52, 0.158, 0.3, head_width=0.01, head_length=0.01, fc='black', ec='black')
+        plt.arrow(0.28, 0.52, 0.157, 0.3, head_width=0.01, head_length=0.01, fc='black', ec='black')
 
         # Axis labels
         ax.text(0.5, 1 + 0.05, 'A (Al₂O₃)', ha='center', fontsize=14)
