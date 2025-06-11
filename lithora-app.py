c23a658806c6fba9e36f789f002be753bc6319be
Etasin16 <e.tasin02@gmail.com>
2025-06-11 20:47:43 +0600

    asdf

--- lithora-app.py
+++ lithora-app.py
@@ -242,7 +242,8 @@ elif st.session_state.page == "cia":
         lines = [(-0.1, 0.85), (0.57, 0.85)]
         x_li, y_li = zip(*lines)
         ax.plot(x_li, y_li, linestyle='--', color='gray')
-        ax.text(-0.001, 0.87, 'strong\nweathering', ha='center', fontsize=7,color = "gray")
+        ax.text(-0.001, 0.87, 'Strong\nweathering', ha='center', fontsize=7,color = "gray")
+        ax.text(0.6, 1, 'Kaolinite', ha='center', fontsize=7,color = "gray")
 
              # Intermediate weathering
         lines = [(-0.1, 0.65), (0.68, 0.65)]
@@ -257,12 +258,14 @@ elif st.session_state.page == "cia":
         x_li, y_li = zip(*lines)
         ax.plot(x_li, y_li, linestyle='--', color='gray')
         ax.text(-0.001, 0.52, 'Weak\nweathering', ha='center', fontsize=7,color = "gray")
-        ax.text(0.51, 0.52, 'average granite', ha='center', fontsize=7,color = "gray")
-        ax.text(0.2, 0.52, 'plahioclase', ha='center', fontsize=7,color = "gray")
-        ax.text(0.8, 0.52, 'k-feldspar', ha='center', fontsize=7,color = "gray")
+        ax.text(0.51, 0.52, 'Average granite', ha='center', fontsize=7,color = "gray")
+        ax.text(0.2, 0.52, 'Plahioclase', ha='center', fontsize=7,color = "gray")
+        ax.text(0.8, 0.52, 'K-feldspar', ha='center', fontsize=7,color = "gray")
+        ax.text(0.3, 0.22, 'Average gabbro', ha='center', fontsize=7,color = "gray")
 
           # Arrows
-        plt.arrow(0.28, 0.52, 0.156, 0.3, head_width=0.01, head_length=0.01, fc='black', ec='red')
+        plt.arrow(0.28, 0.52, 0.145, 0.3, head_width=0.01, head_length=0.01, fc='red', ec='red')
+        plt.arrow(0.48, 0.52, 0.045, 0.2, head_width=0.01, head_length=0.01, fc='red', ec='red')
 
         # Axis labels
         ax.text(0.5, 1 + 0.05, 'A (Al₂O₃)', ha='center', fontsize=14)
@@ -275,7 +278,7 @@ elif st.session_state.page == "cia":
             ax.plot(x, y, marker=marker, color=marker_color, markersize=5)
             if show_labels:
                 ax.text(x + 0.01, y + 0.01, label, fontsize=10)
-        ax.legend()
+        
 
         return fig
 
